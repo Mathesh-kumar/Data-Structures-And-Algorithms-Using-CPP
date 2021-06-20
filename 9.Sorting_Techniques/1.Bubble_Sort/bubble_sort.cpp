5c96b22af122e6fb71648d78e2bdb706e7ef838f
Mathesh-kumar <matheshkumar256@gmail.com>
2021-06-20 09:11:17 +0530

    9.1. bubble sort commit

---
+++ 9.Sorting_Techniques/1.Bubble_Sort/bubble_sort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int* arr, int n)
+{
+    while(true)
+    {
+        bool swap = false;
+        for(int i=0; i<n-1; i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                arr[i]^=arr[i+1];
+                arr[i+1]^=arr[i];
+                arr[i]^=arr[i+1];
+                swap = true;
+            }
+        }
+        if(!swap)
+            break;
+    }
+}
+
+int main()
+{
+    int size;
+    cout<<"Enter size of array : ";
+    cin>>size;
+
+    int *arr = new int(size);
+    cout<<"Enter elements of array : ";
+    for(int i=0; i<size; i++)
+        cin>>arr[i];
+
+    bubbleSort(arr, size);
+
+    cout<<"Sorted output : ";
+    for(int i=0; i<size; i++)
+        cout<<arr[i]<<" ";
+
+    return 0;
+}
