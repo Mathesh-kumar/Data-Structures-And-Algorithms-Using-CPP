7211fe4b5c66cbcb1b8e07dfc0a95209b058389e
Mathesh-kumar <matheshkumar256@gmail.com>
2021-06-20 17:26:00 +0530

    9.3. insertion sort update commit

--- 9.Sorting_Techniques/3.Insertion_Sort/insertion_sort.cpp
+++ 9.Sorting_Techniques/3.Insertion_Sort/insertion_sort.cpp
@@ -5,16 +5,11 @@ void insertionSort(int* arr, int n)
 {
     for(int i=1; i<n; i++)
     {
-        if(arr[i-1]>arr[i])
+        if(arr[i-1] > arr[i])
         {
-            int key = arr[i], j;
-            for(j=i-1; j>-1; j--)
-            {
-                if(arr[j] > key)
-                    arr[j+1] = arr[j];
-                else
-                    break;
-            }
+            int key = arr[i], j=i-1;
+            while(j >= 0 && arr[j] > key)
+                arr[j+1] = arr[j--];
             arr[j+1] = key;
         }
     }
